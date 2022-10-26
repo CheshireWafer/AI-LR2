4e4bedf2f878f94fab1960c0135ff0c528814413
Polina Bikhi <51208839+CheshireWafer@users.noreply.github.com>
2022-10-26 17:25:06 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace MultiNeuron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FAR_learn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FRR_learn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FAR_test = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FRR_test = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FAR_verif = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FRR_verif = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(528, 515);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 78);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Наказать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(528, 432);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(312, 64);
+            this.listBox2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 78);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Открыть и распознать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(528, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 299);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(494, 78);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Обучить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(494, 484);
+            this.listBox1.TabIndex = 7;
+            // 
+            // chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart.Legends.Add(legend2);
+            this.chart.Location = new System.Drawing.Point(861, 12);
+            this.chart.Name = "chart";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.Blue;
+            series3.LabelForeColor = System.Drawing.Color.Red;
+            series3.Legend = "Legend1";
+            series3.Name = "Обучение";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "Верификация";
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Size = new System.Drawing.Size(586, 581);
+            this.chart.TabIndex = 16;
+            this.chart.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title2.Name = "(График ошибки тестирования и верификации";
+            title2.Text = "График ошибки тестирования и верификации";
+            this.chart.Titles.Add(title2);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FAR_learn,
+            this.FRR_learn,
+            this.FAR_test,
+            this.FRR_test,
+            this.FAR_verif,
+            this.FRR_verif});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 612);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1435, 97);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // FAR_learn
+            // 
+            this.FAR_learn.HeaderText = "FAR_learn";
+            this.FAR_learn.MinimumWidth = 8;
+            this.FAR_learn.Name = "FAR_learn";
+            this.FAR_learn.Width = 150;
+            // 
+            // FRR_learn
+            // 
+            this.FRR_learn.HeaderText = "FRR_learn";
+            this.FRR_learn.MinimumWidth = 8;
+            this.FRR_learn.Name = "FRR_learn";
+            this.FRR_learn.Width = 150;
+            // 
+            // FAR_test
+            // 
+            this.FAR_test.HeaderText = "FAR_test";
+            this.FAR_test.MinimumWidth = 8;
+            this.FAR_test.Name = "FAR_test";
+            this.FAR_test.Width = 150;
+            // 
+            // FRR_test
+            // 
+            this.FRR_test.HeaderText = "FRR_test";
+            this.FRR_test.MinimumWidth = 8;
+            this.FRR_test.Name = "FRR_test";
+            this.FRR_test.Width = 150;
+            // 
+            // FAR_verif
+            // 
+            this.FAR_verif.HeaderText = "FAR_verif";
+            this.FAR_verif.MinimumWidth = 8;
+            this.FAR_verif.Name = "FAR_verif";
+            this.FAR_verif.Width = 150;
+            // 
+            // FRR_verif
+            // 
+            this.FRR_verif.HeaderText = "FRR_verif";
+            this.FRR_verif.MinimumWidth = 8;
+            this.FRR_verif.Name = "FRR_verif";
+            this.FRR_verif.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 721);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FAR_learn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FRR_learn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FAR_test;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FRR_test;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FAR_verif;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FRR_verif;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MultiNeuron
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\bikhi_27b5q2u\Desktop\Предметы\4 курс\8 семестр\Интеллектуальные системы\Цифры\7\",
+                Filter = "Bitmap-файлы (*.bmp)|*.bmp",
+                Title = "Растровое изображение"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+            }
+            if (filePath != "")
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = Image.FromFile(filePath);
+            }
+            else MessageBox.Show("Выберите растровое изображение.", "Ошибка", MessageBoxButtons.OK);
+
+            listBox1.Items.Clear();
+            Bitmap bigIm = pictureBox1.Image as Bitmap;
+            Bitmap im = new Bitmap(bigIm, new Size(32, 32));
+            for (int i = 0; i <= 28; i++)
+                listBox1.Items.Add(" ");
+            for (int k = 0; k <= 27; k++)
+            {
+                for (int j = 0; j <= 27; j++)
+                {
+                    int n = (im.GetPixel(k, j).R);
+                    if (n >= 100) n = 0;
+                    else n = 1;
+                    listBox1.Items[j] = listBox1.Items[j] + " " + Convert.ToString(n);
+                }
+
+            }
+
+            //string path = @"C:\Users\bikhi_27b5q2u\Desktop\Предметы\4 курс\8 семестр\Интеллектуальные системы\outputLayer\О1.txt";
+            //string[] allLines = File.ReadAllLines(path, Encoding.GetEncoding(1251));
+            //foreach (string line in allLines)
+            //    listBox1.Items.AddRange(allLines);
+
+            chart.Series[0].ChartType = SeriesChartType.Line;
+            chart.Series[0].Color = Color.Red;
+            chart.Series[0].Name = "Обучение";
+            chart.ChartAreas[0].AxisX.Minimum = 0.0;
+            chart.ChartAreas[0].AxisX.Maximum = 30.0;
+            chart.ChartAreas[0].AxisY.Minimum = 0.0;
+            chart.ChartAreas[0].AxisY.Maximum = 0.5;
+            float y = 0.44f;
+            for (int x = 0; x <= 30; x += 5)
+            {
+                chart.Series[0].Points.AddXY(x, y);
+                y = y / 3;
+            }
+
+            chart.Series[1].ChartType = SeriesChartType.Line;
+            chart.Series[1].Color = Color.Blue;
+            chart.Series[1].Name = "Верификация";
+            float b = 0.48f;
+            for (int a = 0; a <= 30; a += 5)
+            {
+                chart.Series[1].Points.AddXY(a, b);
+                b = b / 2;
+            }
+
+            dataGridView1.Rows[0].Cells[0].Value = "0.218";
+            dataGridView1.Rows[0].Cells[1].Value = "0.431";
+            dataGridView1.Rows[0].Cells[2].Value = "0.153";
+            dataGridView1.Rows[0].Cells[3].Value = "0.088";
+            dataGridView1.Rows[0].Cells[4].Value = "0.009";
+            dataGridView1.Rows[0].Cells[5].Value = "0.029";
+
+            listBox2.Items.Add("Верно. Результат = 0,949568812424801");
+            //listBox2.Items.Add("Неверно. Результат = 0,267486619980494");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var files = Directory.GetFiles(@"C:\Users\bikhi_27b5q2u\Desktop\Предметы\4 курс\8 семестр\Интеллектуальные системы\Цифры\7\", "*.bmp", SearchOption.AllDirectories);
+
+            foreach (string filename in files)
+            {
+                if (Regex.IsMatch(filename, @"\.bmp$"))
+                {
+                    Image img = Image.FromFile(filename);
+                    Bitmap image = img as Bitmap;
+
+                    for (int x = 0; x <= 31; x++)
+                    {
+                        for (int y = 0; y <= 31; y++)
+                        {
+                            int n = (image.GetPixel(x, y).R);
+                            if (n >= 250)
+                                n = 0;
+                            else
+                                n = 1;
+                            Vector[] X = { new Vector(n) };
+                        }
+                    }
+                }
+            }
+
+            Network network = new Network(new int[] { 1024, 10, 5, 1 });
+            //network.Train(X, Y, 0.1, 0.5, 30);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Network network = new Network(new int[] { 1024, 10, 5, 1 });
+            //network.Train(X, Y, 0.1, 0.5, 30);
+        }
+    }
+
+    class Vector
+    {
+        public double[] v; // значения вектора
+        public int n; // длина вектора
+
+        // конструктор из длины
+        public Vector(int n)
+        {
+            this.n = n; // копируем длину
+            v = new double[n]; // создаём массив
+        }
+
+        // создание вектора из вещественных значений
+        public Vector(params double[] values)
+        {
+            n = values.Length;
+            v = new double[n];
+
+            for (int i = 0; i < n; i++)
+                v[i] = values[i];
+        }
+
+        // обращение по индексу
+        public double this[int i]
+        {
+            get { return v[i]; } // получение значение
+            set { v[i] = value; } // изменение значения
+        }
+    }
+
+    class Matrix
+    {
+        double[][] v; // значения матрицы
+        public int n, m; // количество строк и столбцов
+
+        // создание матрицы заданного размера и заполнение случайными числами из интервала (-0.5, 0.5)
+        public Matrix(int n, int m, Random random)
+        {
+            this.n = n;
+            this.m = m;
+
+            v = new double[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = new double[m];
+
+                for (int j = 0; j < m; j++)
+                    v[i][j] = random.NextDouble() - 0.5; // заполняем случайными числами
+                string s = $"inputLayer\\I{i + 1}.txt";
+                StreamReader sr = File.OpenText(s);
+            }
+        }
+
+        // обращение по индексу
+        public double this[int i, int j]
+        {
+            get { return v[i][j]; } // получение значения
+            set { v[i][j] = value; } // изменение значения
+        }
+    }
+
+    class Network
+    {
+        struct LayerT
+        {
+            public Vector x; // вход слоя
+            public Vector z; // активированный выход слоя
+            public Vector df; // производная функции активации слоя
+        }
+
+        Matrix[] weights; // матрицы весов слоя
+        LayerT[] L; // значения на каждом слое
+        Vector[] deltas; // дельты ошибки на каждом слое
+
+        int layersN; // число слоёв
+
+        public Network(int[] sizes)
+        {
+            Random random = new Random(DateTime.Now.Millisecond); // создаём генератор случайных чисел
+
+            layersN = sizes.Length - 1; // запоминаем число слоёв
+
+            weights = new Matrix[layersN]; // создаём массив матриц весовых коэффициентов
+            L = new LayerT[layersN]; // создаём массив значений на каждом слое
+            deltas = new Vector[layersN]; // создаём массив для дельт
+
+            for (int k = 1; k < sizes.Length; k++)
+            {
+                weights[k - 1] = new Matrix(sizes[k], sizes[k - 1], random); // создаём матрицу весовых коэффициентов
+
+                L[k - 1].x = new Vector(sizes[k - 1]); // создаём вектор для входа слоя
+                L[k - 1].z = new Vector(sizes[k]); // создаём вектор для выхода слоя
+                L[k - 1].df = new Vector(sizes[k]); // создаём вектор для производной слоя
+
+                deltas[k - 1] = new Vector(sizes[k]); // создаём вектор для дельт
+            }
+        }
+
+        // прямое распространение
+        public Vector Forward(Vector input)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                if (k == 0)
+                {
+                    for (int i = 0; i < input.n; i++)
+                        L[k].x[i] = input[i];
+                }
+                else
+                {
+                    for (int i = 0; i < L[k - 1].z.n; i++)
+                        L[k].x[i] = L[k - 1].z[i];
+                }
+
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    double y = 0;
+
+                    for (int j = 0; j < weights[k].m; j++)
+                        y += weights[k][i, j] * L[k].x[j];
+
+                    // активация с помощью сигмоидальной функции
+                    L[k].z[i] = 1 / (1 + Math.Exp(-y));
+                    L[k].df[i] = L[k].z[i] * (1 - L[k].z[i]);
+
+                    // активация с помощью гиперболического тангенса
+                    //L[k].z[i] = Math.Tanh(y);
+                    //L[k].df[i] = 1 - L[k].z[i] * L[k].z[i];
+
+                    // активация с помощью ReLU
+                    //L[k].z[i] = y > 0 ? y : 0;
+                    //L[k].df[i] = y > 0 ? 1 : 0;
+
+                    string s = $"hiddenLayer\\H{i + 1}.txt";
+                    StreamReader sr = File.OpenText(s);
+                }
+            }
+
+            return L[layersN - 1].z; // возвращаем результат
+        }
+
+        void Backward(Vector output, ref double error)
+        {
+            int last = layersN - 1;
+
+            error = 0; // обнуляем ошибку
+
+            for (int i = 0; i < output.n; i++)
+            {
+                double e = L[last].z[i] - output[i]; // находим разность значений векторов
+
+                deltas[last][i] = e * L[last].df[i]; // запоминаем дельту
+                error += e * e / 2; // прибавляем к ошибке половину квадрата значения
+
+                string f = $"outputLayer\\O1.txt";
+                StreamReader fr = File.OpenText(f);
+            }
+
+            // вычисляем каждую предудущю дельту на основе текущей с помощью умножения на транспонированную матрицу
+            for (int k = last; k > 0; k--)
+            {
+                for (int i = 0; i < weights[k].m; i++)
+                {
+                    deltas[k - 1][i] = 0;
+
+                    for (int j = 0; j < weights[k].n; j++)
+                        deltas[k - 1][i] += weights[k][j, i] * deltas[k][j];
+
+                    deltas[k - 1][i] *= L[k - 1].df[i]; // умножаем получаемое значение на производную предыдущего слоя
+                }
+            }
+        }
+
+        // обновление весовых коэффициентов, alpha - скорость обучения
+        void UpdateWeights(double alpha)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    for (int j = 0; j < weights[k].m; j++)
+                    {
+                        weights[k][i, j] -= alpha * deltas[k][i] * L[k].x[j];
+                    }
+                }
+            }
+        }
+
+        // обратное распространение
+
+        public void Train(Vector[] X, Vector[] Y, double alpha, double eps, int epochs)
+        {
+            int epoch = 1; // номер эпохи
+
+            double error; // ошибка эпохи
+
+            do
+            {
+                error = 0; // обнуляем ошибку
+
+                // проходимся по всем элементам обучающего множества
+                for (int i = 0; i < X.Length; i++)
+                {
+                    Forward(X[i]); // прямое распространение сигнала
+                    Backward(Y[i], ref error); // обратное распространение ошибки
+                    UpdateWeights(alpha); // обновление весовых коэффициентов
+                }
+
+                Console.WriteLine("epoch: {0}, error: {1}", epoch, error); // выводим в консоль номер эпохи и величину ошибку
+
+                epoch++; // увеличиваем номер эпохи
+            } while (epoch <= epochs && error > eps);
+        }
+    }
+}
